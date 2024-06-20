97868f895c674972455d8f7ee99d007e3aa65b28
[AI11] 김민지 <kkimming__@naver.com>
2024-06-20 17:16:42 +0900

    feat: login post fetch 작성

--- src/views/components/UserMVP/scripts/login.js
+++ src/views/components/UserMVP/scripts/login.js
@@ -2,21 +2,37 @@ const loginForm = document.getElementById("login-form");
 const loginInputEmail = document.getElementById("login-email");
 const loginInputPassword = document.getElementById("login-password");
 const loginButton = document.getElementById("login-btn");
-const joinButton = document.getElementById("join-btn");
+const joinButton = document.getElementById("join-btn-at-login");
 
 // 로그인 버튼 submit event
 function onLoginSubmit(event) {
-  console.log(event);
   event.preventDefault();
-  console.log(loginInputEmail.value);
-  console.log(loginInputPassword.value);
-  const userInfo = {
-    email: loginInputEmail.value,
-    password: loginInputPassword.value,
-  };
-  console.log(userInfo);
+  const useremail = loginInputEmail.value;
+  const userpassword = loginInputPassword.value;
+  console.log(useremail);
+  console.log(userpassword);
+  fetchPostLogin(useremail, userpassword);
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
 
-// 회원가입 버튼 click event
+function fetchPostLogin(useremail, userpassword) {
+  fetch("./auth/join", {
+    method: "POST",
+    body: JSON.stringify({
+      email: useremail,
+      password: userpassword,
+    }),
+  })
+    .then((res) => {
+      res.json();
+    })
+    .then((res) => {
+      if (res.success) {
+        alert("로그인 성공!");
+      }
+    });
+}
+
+// 회원가입 버튼 click event fetch
+function onJoinBtnClickAtLogin(event) {}
