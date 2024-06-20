f2b249681831c4666debfa79f2b685d5fb9b726f
[AI11] 김민지 <kkimming__@naver.com>
2024-06-20 16:30:45 +0900

    fect: login 회원가입 btn event

--- src/views/components/UserMVP/scripts/login.js
+++ src/views/components/UserMVP/scripts/login.js
@@ -2,7 +2,9 @@ const loginForm = document.getElementById("login-form");
 const loginInputEmail = document.getElementById("login-email");
 const loginInputPassword = document.getElementById("login-password");
 const loginButton = document.getElementById("login-btn");
+const joinButton = document.getElementById("join-btn");
 
+// 로그인 버튼 submit event
 function onLoginSubmit(event) {
   console.log(event);
   event.preventDefault();
@@ -16,3 +18,5 @@ function onLoginSubmit(event) {
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
+
+// 회원가입 버튼 click event
