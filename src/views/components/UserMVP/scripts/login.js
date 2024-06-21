50501e3a60f393c8c72e451d5961344ef9470565
[AI11] 김민지 <kkimming__@naver.com>
2024-06-21 14:35:31 +0900

    fix: 로그인 fetch res 수정 중

--- src/views/components/UserMVP/scripts/login.js
+++ src/views/components/UserMVP/scripts/login.js
@@ -4,6 +4,8 @@ const loginInputPassword = document.getElementById("login-password");
 const loginButton = document.getElementById("login-btn");
 const joinButton = document.getElementById("join-btn-at-login");
 
+console.log(loginForm);
+
 // 로그인 버튼 submit event
 function onLoginSubmit(event) {
   event.preventDefault();
@@ -17,7 +19,7 @@ function onLoginSubmit(event) {
 loginForm.addEventListener("submit", onLoginSubmit);
 
 function fetchPostLogin(useremail, userpassword) {
-  fetch("./auth/join", {
+  fetch("api/auth/join", {
     method: "POST",
     body: JSON.stringify({
       email: useremail,
@@ -25,14 +27,33 @@ function fetchPostLogin(useremail, userpassword) {
     }),
   })
     .then((res) => {
-      res.json();
+      if (!res.ok) {
+        console.log("response error");
+        throw new Error("네트워크 응답이 올바르지 않습니다.");
+      }
+      return res.json();
     })
+    // 로그인 응답 어떻게 들어오는지 보고 쓰기
     .then((res) => {
-      if (res.success) {
-        alert("로그인 성공!");
+      if (res) {
+        console.log(res);
       }
     });
 }
 
-// 회원가입 버튼 click event fetch
-function onJoinBtnClickAtLogin(event) {}
+// text 정보만 사용 가능
+const nonBodyMethods = ["GET", "DELETE"];
+const baseApi = async (url, method, data = {}, headers) => {
+  const userId = localStorage.getItem("userId");
+  try {
+    if (nonBodyMethods.includes(method)) {
+      // GET, DELETE 요청이면 바디 없이 fetch
+      return await fetch(url, { method, headers });
+    }
+    // POST, PUT 요청이면 바디 있는 fetch
+    return await fetch(url, { method, body: JSON.stringify(data) });
+  } catch (err) {
+    console.log("login - try error");
+    console.log(err);
+  }
+};
