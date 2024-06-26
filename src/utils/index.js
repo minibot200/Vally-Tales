2d4bdf6cbc6fd663119a30c2733bb14324730e22
[AI11] 김민지 <kkimming__@naver.com>
2024-06-26 17:09:11 +0900

    branch clear

--- src/utils/index.js
+++
@@ -1,9 +0,0 @@
-const checkAuthorization = require('./checkAuthorization');
-const generateRandomNumbers = require('./generateRandomNumbers');
-const sendEmail = require('./sendEmail');
-
-module.exports = {
-    checkAuthorization,
-    generateRandomNumbers,
-    sendEmail,
-};
