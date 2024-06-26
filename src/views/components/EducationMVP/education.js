5bb6b1e6d59647f0e6284c9aad5bfc9cf13cb063
[AI11] 김민지 <kkimming__@naver.com>
2024-06-26 18:43:35 +0900

    fix: edu get 수정

--- src/views/components/EducationMVP/education.js
+++ src/views/components/EducationMVP/education.js
@@ -10,11 +10,13 @@ const startDateInput = document.getElementById("startDate");
 const endDateInput = document.getElementById("endDate");
 // const educationList = [];
 
-// 조회 데이터 저장
-async function loadData(userId) {
+async function loadData() {
   //get api 호출 후 data 저장
+  const userId = await getUserId();
+  console.log(userId);
   const userData = await loadEducationData(userId);
   const educationList = userData;
+  console.log(educationList);
 
   let educationEditingIndex = -1;
   addEducationBtn.addEventListener("click", () => {
@@ -23,163 +25,133 @@ async function loadData(userId) {
     toggleEducationForm();
   });
 
+  // 리스트를 묶으면 안됨
   updateEducationList(educationList);
-}
 
-loadData(localStorage.getItem("userId"));
-
-// let educationEditingIndex = -1;
-// addEducationBtn.addEventListener("click", () => {
-//   educationEditingIndex = -1;
-//   clearEducationForm();
-//   toggleEducationForm();
-// });
-
-document.getElementById("cancelBtn").addEventListener("click", () => {
-  clearEducationForm();
-  toggleEducationForm();
-});
-
-document.getElementById("confirmBtn").addEventListener("click", (e) => {
-  e.preventDefault();
-  const degreeElement = document.querySelector('input[name="degree"]:checked');
-  const degree = degreeElement ? degreeElement.value : "";
-
-  if (
-    schoolNameInput.value &&
-    majorInput.value &&
-    degree &&
-    startDateInput.value &&
-    endDateInput.value
-  ) {
-    if (educationEditingIndex === -1) {
-      // 이 전에 없으면 추가
-      educationList.push({
-        schoolName: schoolNameInput.value,
-        major: majorInput.value,
-        degree: degree,
-        startDate: startDate.value,
-        endDate: endDate.value,
-      });
-    } else {
-      // 있으면 list의 어떤 인덱스에 추가
-      educationList[educationEditingIndex] = {
-        schoolName: schoolNameInput.value,
-        major: majorInput.value,
-        degree: degree,
-        startDate: startDate.value,
-        endDate: endDate.value,
-      };
-      educationEditingIndex = -1;
-    }
-
-    updateEducationList();
+  document.getElementById("cancelBtn").addEventListener("click", () => {
     clearEducationForm();
     toggleEducationForm();
-  } else {
-    alert("모든 필드를 입력해 주세요.");
-  }
-});
-
-function updateEducationList(educationList) {
-  const fragment = document.createDocumentFragment();
-  const educationListDiv = document.getElementById("educationList");
-  educationListDiv.innerHTML = "";
-
-  educationList.forEach((item, index) => {
-    const educationItemDiv = document.createElement("div");
-    educationItemDiv.className = "education-item";
-
-    const educationText = document.createElement("span");
-    educationText.innerText = `${item.schoolName} ${item.major} (${item.degree}) ${item.startDate}~${item.endDate}`;
-    educationItemDiv.appendChild(educationText);
-
-    const editBtn = document.createElement("button");
-    editBtn.className = "edit-btn btn btn-link";
-    editBtn.innerText = "편집";
-    editBtn.addEventListener("click", () => {
-      editEducation(index);
-    });
-    educationItemDiv.appendChild(editBtn);
-
-    const deleteBtn = document.createElement("button");
-    deleteBtn.className = "delete-btn btn btn-link";
-    deleteBtn.innerText = "삭제";
-    deleteBtn.addEventListener("click", () => {
-      deleteEducation(index);
-    });
-    educationItemDiv.appendChild(deleteBtn);
-
-    fragment.appendChild(educationItemDiv);
   });
-  educationListDiv.appendChild(fragment);
-}
 
-function editEducation(index) {
-  educationEditingIndex = index;
-  const item = educationList[index];
-  schoolNameInput.value = item.schoolName;
-  majorInput.value = item.major;
-  const degrees = document.getElementsByName("degree");
-  degrees.forEach((degree) => {
-    degree.checked = degree.value === item.degree;
-    startDateInput.value = item.startDate;
-    endDateInput.value = item.endDate;
+  document.getElementById("confirmBtn").addEventListener("click", (e) => {
+    e.preventDefault();
+    const degreeElement = document.querySelector(
+      'input[name="degree"]:checked'
+    );
+    const degree = degreeElement ? degreeElement.value : "";
+
+    if (
+      schoolNameInput.value &&
+      majorInput.value &&
+      degree &&
+      startDateInput.value &&
+      endDateInput.value
+    ) {
+      if (educationEditingIndex === -1) {
+        // 이 전에 없으면 추가
+        educationList.push({
+          schoolName: schoolNameInput.value,
+          major: majorInput.value,
+          degree: degree,
+          startDate: startDate.value,
+          endDate: endDate.value,
+        });
+        //post
+      } else {
+        // 있으면 수정
+        educationList[educationEditingIndex] = {
+          schoolName: schoolNameInput.value,
+          major: majorInput.value,
+          degree: degree,
+          startDate: startDate.value,
+          endDate: endDate.value,
+        };
+        // put
+        educationEditingIndex = -1;
+      }
+
+      updateEducationList();
+      clearEducationForm();
+      toggleEducationForm();
+    } else {
+      alert("모든 필드를 입력해 주세요.");
+    }
   });
-  toggleEducationForm(true);
-}
 
-function deleteEducation(index) {
-  educationList.splice(index, 1);
-  updateEducationList();
-}
+  function updateEducationList(educationList) {
+    const fragment = document.createDocumentFragment();
+    const educationListDiv = document.getElementById("educationList");
+    educationListDiv.innerHTML = "";
 
-function clearEducationForm() {
-  schoolNameInput.value = "";
-  majorInput.value = "";
-  const degrees = document.getElementsByName("degree");
-  degrees.forEach((degree) => {
-    degree.checked = false;
-  });
-  startDateInput.value = "";
-  endDateInput.value = "";
-}
+    educationList.forEach((item, index) => {
+      console.log(index);
+      const educationItemDiv = document.createElement("div");
+      educationItemDiv.className = "education-item";
 
-function toggleEducationForm(forceOpen = false) {
-  if (forceOpen) {
-    showElement(educationForm);
-  } else {
-    educationForm.style.display =
-      educationForm.style.display === "block" ? "none" : "block";
-  }
-}
+      const educationText = document.createElement("span");
+      educationText.innerText = `${item.schoolName} ${item.major} (${item.degree}) ${item.startDate}~${item.endDate}`;
+      educationItemDiv.appendChild(educationText);
 
-// 학력 조회 함수
-const getEducations = async (userId) => {
-  try {
-    const response = await fetch(`/api/users/${userId}/educations`, {
-      method: "GET",
-      credentials: "include", // 세션 쿠키 포함
+      const editBtn = document.createElement("button");
+      editBtn.className = "edit-btn btn btn-link";
+      editBtn.innerText = "편집";
+      editBtn.addEventListener("click", () => {
+        editEducation(index);
+      });
+      educationItemDiv.appendChild(editBtn);
+
+      const deleteBtn = document.createElement("button");
+      deleteBtn.className = "delete-btn btn btn-link";
+      deleteBtn.innerText = "삭제";
+      deleteBtn.addEventListener("click", () => {
+        deleteEducation(index);
+        // 삭제 api
+      });
+      educationItemDiv.appendChild(deleteBtn);
+
+      fragment.appendChild(educationItemDiv);
     });
-    const result = await response.json();
-    console.log(result);
-  } catch (error) {
-    console.error("Error:", error);
+    educationListDiv.appendChild(fragment);
   }
-};
 
-// 학력 추가 함수
-const addEducation = async (userId, educationData) => {
-  try {
-    const response = await fetch(`/api/users/${userId}/educations`, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(educationData),
-      credentials: "include", // 세션 쿠키 포함
+  function editEducation(index) {
+    educationEditingIndex = index;
+    const item = educationList[index];
+    schoolNameInput.value = item.schoolName;
+    majorInput.value = item.major;
+    const degrees = document.getElementsByName("degree");
+    degrees.forEach((degree) => {
+      degree.checked = degree.value === item.degree;
+      startDateInput.value = item.startDate;
+      endDateInput.value = item.endDate;
     });
-    const result = await response.json();
-    console.log(result);
-  } catch (error) {
-    console.error("Error:", error);
+    toggleEducationForm(true);
   }
-};
+
+  function deleteEducation(index) {
+    educationList.splice(index, 1);
+    updateEducationList();
+  }
+
+  function clearEducationForm() {
+    schoolNameInput.value = "";
+    majorInput.value = "";
+    const degrees = document.getElementsByName("degree");
+    degrees.forEach((degree) => {
+      degree.checked = false;
+    });
+    startDateInput.value = "";
+    endDateInput.value = "";
+  }
+
+  function toggleEducationForm(forceOpen = false) {
+    if (forceOpen) {
+      showElement(educationForm);
+    } else {
+      educationForm.style.display =
+        educationForm.style.display === "block" ? "none" : "block";
+    }
+  }
+}
+
+loadData();
