a6ceef34111ed01d682af81eebdc829527806b11
[AI11] 김민지 <kkimming__@naver.com>
2024-06-28 02:41:11 +0900

    style: console.log 삭제

--- src/views/components/logout.js
+++ src/views/components/logout.js
@@ -6,9 +6,7 @@ function fetchPostLogout(event) {
       "Content-Type": "application/json",
     },
   }).then((res) => {
-    console.log(res);
     if (res.redirected) {
-      console.log("로그인페이지로 이동");
       alert(`로그아웃!`);
       window.location.href = res.url;
       return;
