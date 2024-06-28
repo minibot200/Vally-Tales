29cc4613840ecf261f0f4c1d3c214b0c43df6d65
[AI11] 김민지 <kkimming__@naver.com>
2024-06-28 15:51:50 +0900

    디렉토리 이동

--- src/views/components/UserMVP/logout.js
+++ src/views/components/UserMVP/logout.js
@@ -1,14 +1,15 @@
 function fetchPostLogout(event) {
   event.preventDefault();
+  localStorage.clear();
   fetch("/api/auth/logout", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
   }).then((res) => {
-    if (res.redirected) {
+    if (res.status === 204) {
       alert(`로그아웃!`);
-      window.location.href = res.url;
+      window.location.href = "/";
       return;
     } else {
       alert(`error : 오류가 발생했습니다.`);
