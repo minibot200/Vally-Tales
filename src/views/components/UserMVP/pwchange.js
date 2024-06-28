5f3c051f1ef0b8c275da8afbceb3de9f3351ae86
[AI11] 김민지 <kkimming__@naver.com>
2024-06-28 13:06:26 +0900

    fix: pwchange 리다이렉트 수정 중

--- src/views/components/UserMVP/pwchange.js
+++ src/views/components/UserMVP/pwchange.js
@@ -8,9 +8,21 @@ document
   .addEventListener("submit", function (event) {
     event.preventDefault();
 
+    const oldPassword = document.getElementById("oldPassword").value;
+    const newPassword = document.getElementById("newPassword").value;
+    const confirmPassword = document.getElementById("confirmPassword").value;
+    const messageDiv = document.getElementById("message");
+
+    if (oldPassword === newPassword) {
+      messageDiv.textContent = "이전 비밀번호와 다른 비밀번호를 입력해주세요.";
+      messageDiv.style.color = "gray";
+      return;
+    }
+
     if (oldPassword && newPassword && confirmPassword) {
     } else {
-      return alert("모든 필드를 입력해 주세요.");
+      messageDiv.textContent = "모든 필드를 입력해 주세요.";
+      return;
     }
 
     if (newPassword !== confirmPassword) {
@@ -20,8 +32,7 @@ document
     }
 
     if (newPassword.length < 4) {
-      messageDiv.textContent =
-        "비밀번호가 너무 짧습니다. 4글자 이상 입력해주세요.";
+      messageDiv.textContent = "새 비밀번호를 4글자 이상 입력해주세요.";
       messageDiv.style.color = "gray";
       return;
     }
@@ -39,16 +50,18 @@ document
       }),
     })
       .then((response) => {
-        if (response.ok) {
+        if (response.redirected) {
           alert("비밀번호 변경에 성공했습니다!");
-          window.location.href = res.url;
+          console.log(response);
+          window.location.href = response.url;
           return;
         } else {
-          alert(response.error);
+          messageDiv.textContent = "비밀번호가 틀렸습니다.";
+          messageDiv.style.color = "gray";
         }
       })
       .catch((error) => {
-        console.error("Error:", error);
-        messageDiv.textContent = "비밀번호 변경 중 에러가 발생했습니다.";
+        console.log(response.error);
+        // messageDiv.textContent = "비밀번호 변경 중 에러가 발생했습니다.";
       });
   });
