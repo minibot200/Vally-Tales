ac501fe1cc2b9b6d97fb371dc8dd12ef604a67c8
[AI11]김재영A <goodikjy@naver.com>
2024-06-28 09:58:03 +0900

    Fix: 이름 최대 길이 추가

--- src/middlewares/validationSchemas/userSchema.js
+++ src/middlewares/validationSchemas/userSchema.js
@@ -3,10 +3,12 @@ const Joi = require('joi');
 const userSchema = Joi.object({
     name: Joi.string()
         .min(2)
+        .max(12)
         .required()
         .messages({
           "string.empty": "이름은 공백일 수 없습니다.",
           "string.min": "이름은 최소 2글자 이상입니다.",
+          "string.max": "이름은 최대 12글자 이하입니다.",
           "any.required": "이름은 필수값입니다.",
         }),
 });
